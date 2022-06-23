41464ed570cd9975564ad4e906f65212bf8c7456
mi <>
2022-06-24 08:23:49 +0900

    update

--- EndianConvert.hpp
+++ EndianConvert.hpp
@@ -23,12 +23,12 @@ public:
 
     template<class T>
     static void Conv(T *src, T* dest, int num) {
-        T *tmp = new T(num);
+        T *tmp = new T[num];
         for(int i = num; 0 < i; --i) {
             tmp[i-1] = src[num - i];
         }
 
         memcpy(dest, tmp, num);
-        delete tmp;
+        delete[] tmp;
     }
 };
